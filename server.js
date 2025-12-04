705db09fa5dd0393886b4af18464caf36783a4a7
22-67620-svg <22-67620@g.batstate-u.edu.ph>
2025-12-04 14:47:07 +0000

    Update message

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.write("Hello Cloud!!!");
-  res.write("Welcome to Jhasmine's page.");
+  // Tell the browser we're sending HTML
+  res.writeHead(200, { "Content-Type": "text/html" });
+
+  res.write("<h1>Hello Everyone!</h1>");
+  res.write("<p>Welcome to my page.</p>");
+  res.write("<p>I am <strong>Jhasmine Dimaano</strong> from <strong>BSIT BA - 4102</strong></p>");
+  res.write(`
+    <blockquote>
+      “You only live once, but if you do it right, once is enough.”<br>
+      ― Mae West
+    </blockquote>
+  `);
+
   res.end();
 });
 
